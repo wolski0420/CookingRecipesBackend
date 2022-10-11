b20e9af0b69ddf9869c663c58df1c537d0b5e756
Łukasz Wolski <55734622+wolski0420@users.noreply.github.com>
2022-10-11 22:44:17 +0200

    Transformed application to the version with using Cloud Firestore instead of Hibernate

--- src/main/java/com/app/cooking/recipes/backend/CookingRecipesBackendApplication.java
+++ src/main/java/com/app/cooking/recipes/backend/CookingRecipesBackendApplication.java
@@ -1,12 +1,36 @@
 package com.app.cooking.recipes.backend;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
 
 @SpringBootApplication
 public class CookingRecipesBackendApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ClassLoader classLoader = CookingRecipesBackendApplication.class.getClassLoader();
+
+        File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://cookingrecipes-793c3.firebaseio.com")
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options, "appName");
+        } else {
+            FirebaseApp.initializeApp(options);
+        }
+
         SpringApplication.run(CookingRecipesBackendApplication.class, args);
     }
 
